7ee5dcec2377251c9ec87c935fdb0551b0fe27a1
Marlon <mqchado@gmail.com>
2018-03-05 15:56:28 -0500

    unn simple sout

--- src/javaapplication20/JavaApplication20.java
+++ src/javaapplication20/JavaApplication20.java
@@ -15,7 +15,6 @@ public class JavaApplication20 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola mi primer git");
     }
-    
 }
